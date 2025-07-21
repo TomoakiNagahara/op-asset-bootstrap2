084fd9b50aa224010d43c02cb32b555f73e2e0b2
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-07-21 12:53:01 +0900

    Mov: include > config.php --> admin.php

---
+++ include/admin.php
@@ -0,0 +1,36 @@
+<?php
+/**	op-asset-bootstrap2:/include/admin.php
+ *
+ * @created    2025-07-20
+ * @version    1.0
+ * @package    op-asset-bootstrap2
+ * @author     Tomoaki Nagahara
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+
+/**	namespace
+ *
+ */
+namespace OP;
+
+//	...
+if( OP::isCI() ){
+	return;
+}
+
+//	...
+$admin = Config::Get('admin');
+
+//	...
+if( isset($admin[OP::_ADMIN_IP_]) ){
+	return;
+}
+
+//	...
+$path = __DIR__.'/../template/admin.phtml';
+
+//	...
+include(__DIR__.'/../template/html.phtml');
+
+//	For Eclipse IDE never used notice.
+echo $path;
