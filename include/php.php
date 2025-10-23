3aa8ed396c9f604996655eae91f332efc27aff97
TomoakiMacBookProM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-10-24 01:23:54 +0900

    Add: include > php.php | PHPDoc

---
+++ include/php.php
@@ -0,0 +1,14 @@
+<?php
+/**	op-asset-bootstrap2:/include/php.php
+ *
+ * @rebirth    2025-10-23
+ * @version    1.0
+ * @package    op-asset-bootstrap2
+ * @author     Tomoaki Nagahara
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+
+/**	namespace
+ *
+ */
+namespace OP;
